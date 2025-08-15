9b003681476c62e9d6afd07d4f9c67a904cb7842
AbeerAhmad2003 <abeerahmad159357@gmail.com>
2025-08-15 18:51:40 +0300

    add validator

---
+++ ProductValidator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory_Project
+{
+    public static class ProductValidator
+    {
+        public static bool IsValid(Product product)
+        {
+            return !string.IsNullOrWhiteSpace(product.Name)
+                &&product.Quantity>=0
+                && product.Price>=0;
+        }
+    }
+}
